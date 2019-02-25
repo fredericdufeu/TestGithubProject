eea6890b08d8a5dff62db7b92e4a7a467abedcac
Keitaro Takahashi <neoterize@me.com>
2019-02-25 15:31:33 +0000

    Keitaro

---
+++ TestGithubProject/KeitaroFile.hpp
@@ -0,0 +1,14 @@
+//
+//  KeitaroFile.hpp
+//  TestGithubProject
+//
+//  Created by Keitaro on 25/02/2019.
+//  Copyright © 2019 Frédéric Dufeu. All rights reserved.
+//
+
+#ifndef KeitaroFile_hpp
+#define KeitaroFile_hpp
+
+#include <stdio.h>
+
+#endif /* KeitaroFile_hpp */
