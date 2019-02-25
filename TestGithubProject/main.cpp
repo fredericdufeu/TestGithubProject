8c04831127dc7f1826f851e584c646c0d3d3d6c8
Frédéric Dufeu <fredericdufeu@frederics-macbook-pro.local>
2019-02-25 15:26:43 +0000

    Initial Commit

---
+++ TestGithubProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TestGithubProject
+//
+//  Created by Frédéric Dufeu on 25/02/2019.
+//  Copyright © 2019 Frédéric Dufeu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
