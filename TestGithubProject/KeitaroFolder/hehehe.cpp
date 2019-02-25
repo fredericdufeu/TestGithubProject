ff4f49c3842567cae45b179cd95a68e3e3903b48
Keitaro Takahashi <neoterize@me.com>
2019-02-25 15:32:21 +0000

    george

---
+++ TestGithubProject/KeitaroFolder/hehehe.cpp
@@ -0,0 +1,9 @@
+//
+//  hehehe.cpp
+//  TestGithubProject
+//
+//  Created by Keitaro on 25/02/2019.
+//  Copyright © 2019 Frédéric Dufeu. All rights reserved.
+//
+
+#include "hehehe.hpp"
---
+++ TestGithubProject/KeitaroFolder/hehehe.hpp
@@ -0,0 +1,14 @@
+//
+//  hehehe.hpp
+//  TestGithubProject
+//
+//  Created by Keitaro on 25/02/2019.
+//  Copyright © 2019 Frédéric Dufeu. All rights reserved.
+//
+
+#ifndef hehehe_hpp
+#define hehehe_hpp
+
+#include <stdio.h>
+
+#endif /* hehehe_hpp */
